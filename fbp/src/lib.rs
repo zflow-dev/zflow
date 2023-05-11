fefc37d469a14b9efc4937fecf1368387b262328
darmie <dreakinlaja@gmail.com>
2023-05-11 23:04:09 +0100

    a hacky but better FBP definition parser

--- fbp/src/lib.rs
+++ fbp/src/lib.rs
@@ -81,8 +81,7 @@ enum Kind {
     CompMeta,
     CompName,
     Port,
-    RightPort,
-    LeftPort,
+    LeftRightPort,
     PortIndex,
 }
 
@@ -148,31 +147,118 @@ impl FBPLexer {
             .token(r"^#\s+@.+", Kind::At)
             .token(r"\.", Kind::Dot)
             .token(r"\(([a-zA-Z/\-0-9_]*)?(:.+)?(\))?", Kind::Component)
-            .token(r"[a-zA-Z_][a-zA-Z.0-9_]*", Kind::Name)
+            .token(r"[a-zA-Z_][a-zA-Z0-9_\-]*", Kind::Name)
             // .token(r"->\s+[a-zA-Z_][a-zA-Z.0-9_]*", Kind::RightPort)
             .token(r"[0-9]+", Kind::PortIndex)
             .ignore(r"\n+")
-            .token(r"(INPORT|OUTPORT)=([a-zA-Z_][a-zA-Z0-9_\-]*)(\.)(([a-zA-Z_][a-zA-Z.0-9_]*)(:)([a-zA-Z_][a-zA-Z.0-9_]*))", Kind::InPortOutPort)
+            .token(
+                r"(INPORT|OUTPORT)=([a-zA-Z_][a-zA-Z0-9_\-]*)",
+                Kind::InPortOutPort,
+            )
+            .token(
+                r"([a-zA-Z_][a-zA-Z.0-9_]*)(:)([a-zA-Z_][a-zA-Z.0-9_]*)",
+                Kind::LeftRightPort,
+            )
             .ignore(r"\s+")
-            .build().expect("expected to parse source");
+            .build()
+            .expect("expected to parse source");
 
-        
         let mut atoms = lexer.tokens(source).collect::<Vec<_>>();
 
         let mut tokens = Vec::new();
 
         loop {
             match atoms.clone().as_slice() {
-                [Token{kind: Kind::Doc, span, text}, ..] => {
+                [Token{kind: Kind::IIPChar, span, text:iip}, ..] =>{
+                    atoms.remove(0);
+                    tokens.push(FBPToken::IIPChar(iip.to_string().replace("'", "").to_string()));
+                }
+                [Token {
+                    kind: Kind::InPortOutPort,
+                    span,
+                    text,
+                }, ..] => {
+                    let first_operands = text.split("=").collect::<Vec<_>>();
+                    let operand = first_operands[0];
+                    let node = first_operands[1];
+
+                    let mut node = NodeInfo {
+                        name: node.to_string(),
+                        port: None,
+                    };
+                    atoms.remove(0);
+                    match atoms.clone().as_slice() {
+                        [Token {
+                            kind: Kind::Dot,
+                            span,
+                            text: name,
+                        }, ..] => {
+                            atoms.remove(0);
+
+                            match atoms.clone().as_slice() {
+                                [Token {
+                                    kind: Kind::LeftRightPort,
+                                    span,
+                                    text: name,
+                                }, ..] => {
+                                    let operands = name.split(":").collect::<Vec<_>>();
+                                    let left_port = operands[0];
+                                    let right_port = operands[1];
+                                    node.port = Some(PortInfo {
+                                        name: left_port.to_string(),
+                                        index: None,
+                                    });
+                                    atoms.remove(0);
+
+                                    match operand {
+                                        "INPORT" => {
+                                            tokens.push(FBPToken::Inport(
+                                                node,
+                                                PortInfo {
+                                                    name: right_port.to_string(),
+                                                    index: None,
+                                                },
+                                            ));
+                                        }
+                                        "OUTPORT" => {
+                                            tokens.push(FBPToken::Outport(
+                                                node,
+                                                PortInfo {
+                                                    name: right_port.to_string(),
+                                                    index: None,
+                                                },
+                                            ));
+                                        }
+                                        _ => {}
+                                    }
+                                }
+                                [..] => {}
+                            }
+                        }
+                        [..] => {}
+                    }
+                }
+                [Token {
+                    kind: Kind::Doc,
+                    span,
+                    text,
+                }, ..] => {
                     atoms.remove(0);
                     tokens.push(FBPToken::Doc(text.to_string()));
-                },
-                [Token{kind: Kind::At, span, text}, ..] => {
+                }
+                [Token {
+                    kind: Kind::At,
+                    span,
+                    text,
+                }, ..] => {
                     let text = text.to_string();
                     let text = text.replace("#", "");
                     let annotations = text.trim().split(" ").collect::<Vec<_>>();
                     atoms.remove(0);
-                    tokens.push(FBPToken::Annotation(annotations[0].replace("@", "").to_string(), annotations[1].to_string()))
+                    tokens.push(FBPToken::Annotation(
+                        annotations[0].replace("@", "").to_string(),
+                        annotations[1].to_string(),
+                    ))
                 }
                 [Token {
                     kind: Kind::Name,
@@ -180,12 +266,17 @@ impl FBPLexer {
                     text: name,
                 }, ..] => {
                     atoms.remove(0);
+
                     match atoms.clone().as_slice() {
                         [Token {
                             kind: Kind::Component,
                             span: span2,
                             text: comp,
                         }, ..] => {
+                            let mut node = NodeInfo {
+                                name: name.to_string(),
+                                port: None,
+                            };
                             atoms.remove(0);
                             let component = comp.replace("(", "").replace(")", "");
                             let component = component.split(":").collect::<Vec<_>>();
@@ -194,19 +285,46 @@ impl FBPLexer {
                             if component.len() > 1 {
                                 meta = component[1].to_string();
                             }
-                            if comp.starts_with('(') &&  !comp.ends_with(')')  {
+                            if comp.starts_with('(') && !comp.ends_with(')') {
                                 if atoms[1].kind == Kind::BrClose {
                                     atoms.remove(0);
                                 } else {
-                                    return Err(Error::Syntax(format!("Opened bracket should be closed for component at {} <==", comp)));
+                                    return Err(Error::Syntax(format!(
+                                        "Opened bracket should be closed for component at {} <==",
+                                        comp
+                                    )));
                                 }
-                                
                             }
+
+                            if atoms.len() > 0 {
+                                if atoms[0].kind == Kind::Name && atoms[1].kind == Kind::Arrow {
+                                    let port = atoms[0].text;
+                                    let mut port_info = PortInfo {
+                                        name: port.to_string(),
+                                        index: None,
+                                    };
+                                    match atoms.clone().as_slice() {
+                                        [Token {
+                                            kind: Kind::PortIndex,
+                                            span,
+                                            text: index,
+                                        }, ..] => {
+                                            atoms.remove(0);
+                                            let port_index = index
+                                                .parse::<usize>()
+                                                .expect("expected port index to be an integer");
+                                            port_info.index = Some(port_index);
+                                            node.port = Some(port_info.clone());
+                                        }
+                                        [..] => {
+                                            node.port = Some(port_info.clone());
+                                        }
+                                    }
+                                }
+                            }
+
                             tokens.push(FBPToken::Node(
-                                NodeInfo {
-                                    name: name.to_string(),
-                                    port: None,
-                                },
+                                node,
                                 Some(Box::new(FBPToken::Component(
                                     component_name.to_string(),
                                     meta,
@@ -218,56 +336,43 @@ impl FBPLexer {
                             span,
                             text: port,
                         }, ..] => {
+                            let mut node = NodeInfo {
+                                name: name.to_string(),
+                                port: None,
+                            };
                             atoms.remove(0);
                             let mut port_info = PortInfo {
                                 name: port.to_string(),
                                 index: None,
                             };
-                            loop {
-                                match atoms.clone().as_slice() {
-                                    [Token {
-                                        kind: Kind::PortIndex,
-                                        span,
-                                        text: index,
-                                    }, ..] => {
-                                        atoms.remove(0);
-                                        let port_index = index
-                                            .parse::<usize>()
-                                            .expect("expected port index to be an integer");
-                                        port_info.index = Some(port_index);
-                                    }
-                                    [..] => {
-                                        break;
-                                    }
+                            match atoms.clone().as_slice() {
+                                [Token {
+                                    kind: Kind::PortIndex,
+                                    span,
+                                    text: index,
+                                }, ..] => {
+                                    atoms.remove(0);
+                                    let port_index = index
+                                        .parse::<usize>()
+                                        .expect("expected port index to be an integer");
+                                    port_info.index = Some(port_index);
+                                    node.port = Some(port_info.clone());
+                                }
+                                [..] => {
+                                    node.port = Some(port_info.clone());
                                 }
                             }
-                       
-                            tokens.push(FBPToken::Node(
-                                NodeInfo {
-                                    name: name.to_string(),
-                                    port: Some(port_info),
-                                },
-                                None
-                                )
-                            );
+
+                            tokens.push(FBPToken::Node(node, None));
                         }
-                        [..] => {
-                            tokens.push(FBPToken::Node(
-                                NodeInfo {
-                                    name: name.to_string(),
-                                    port: None,
-                                },
-                                None)
-                            );
-                        }
+                        [..] => {}
                     }
                 }
                 [Token {
                     kind: Kind::Arrow,
-                    span:_,
-                    text:_,
-                },
-                Token {
+                    span: _,
+                    text: _,
+                }, Token {
                     kind: Kind::Name,
                     span,
                     text: port,
@@ -278,30 +383,29 @@ impl FBPLexer {
                         name: port.to_string().replace("-> ", ""),
                         index: None,
                     };
-                    loop {
-                        match atoms.clone().as_slice() {
-                            [Token {
-                                kind: Kind::PortIndex,
-                                span,
-                                text: index,
-                            }, ..] => {
-                                atoms.remove(0);
-                                let port_index = index
-                                    .parse::<usize>()
-                                    .expect("expected port index to be an integer");
-                                port_info.index = Some(port_index);
-                            }
-                            [..] => {
-                                break;
-                            }
+
+                    match atoms.clone().as_slice() {
+                        [Token {
+                            kind: Kind::PortIndex,
+                            span,
+                            text: index,
+                        }, ..] => {
+                            atoms.remove(0);
+                            let port_index = index
+                                .parse::<usize>()
+                                .expect("expected port index to be an integer");
+                            port_info.index = Some(port_index);
+                        }
+                        [..] => {
+
                         }
                     }
+
                     tokens.push(FBPToken::Arrow);
                     tokens.push(FBPToken::Port(port_info));
                 }
                 [..] => {
                     if !atoms.is_empty() {
-                        println!("{:?}", atoms[0]);
                         atoms.remove(0);
                     } else {
                         break;
@@ -312,60 +416,6 @@ impl FBPLexer {
 
         Ok(tokens)
     }
-
-    // fn node_component(atoms: &[Token<Kind>]) -> Option<FBPToken> {
-    //     match atoms {
-    //         [Token {
-    //             kind: Kind::BrOpen,
-    //             span,
-    //             text: _,
-    //         }, Token {
-    //             kind: Kind::CompName,
-    //             span: _,
-    //             text,
-    //         }, Token {
-    //             kind: Kind::BrClose,
-    //             span: _,
-    //             text: _,
-    //         }, ..] => {
-    //             return Some(FBPToken::Component(text.to_string(), "".to_owned()));
-    //         }
-    //         [Token {
-    //             kind: Kind::BrOpen,
-    //             span,
-    //             text: _,
-    //         }, Token {
-    //             kind: Kind::CompName,
-    //             span: _,
-    //             text,
-    //         }, Token {
-    //             kind: Kind::CompMeta,
-    //             span: _,
-    //             text: meta,
-    //         }, Token {
-    //             kind: Kind::BrClose,
-    //             span: _,
-    //             text: _,
-    //         }, ..] => {
-    //             return Some(FBPToken::Component(text.to_string(), meta.to_string()));
-    //         }
-    //         // [
-    //         //     Token {
-    //         //         kind: Kind::CompName,
-    //         //         span: _,
-    //         //         text,
-    //         //     }
-    //         // ] => {
-    //         //     return Some(FBPToken::Component(text.to_string(), text.to_string()));
-    //         // }
-    //         [..] => {
-    //             if atoms[2].kind != Kind::BrClose {
-    //                 panic!("Brack left open");
-    //             }
-    //             return None;
-    //         }
-    //     }
-    // }
 }
 
 impl FBP {
@@ -386,12 +436,20 @@ mod tests {
         OUTPORT=Process.OUT:RESULT
         Read(ReadFile) OUT -> IN Process(Output:key=value)
         Forward OUT -> IN Log(core/console)
+        'somefile.txt' -> SOURCE Read(ReadFile) OUT -> IN Split(SplitStr)
+        Split OUT -> IN Count(Counter) COUNT -> IN Display(Output)
+        Read() ERROR -> IN Display()
+        INPORT=Read.IN:FILENAME
+        'somefile.txt' -> SOURCE Read(ReadFile:main)
+        Read() OUT -> IN Split(SplitStr:main)
+        Split() OUT -> IN Count(Counter:main)
+        Count() COUNT -> IN Display(Output:main)
+        Read() ERROR -> IN Display()
+        Read() OUT -> IN Split(SplitStr:foo=bar,baz=123)
         ",
-        ).expect("Syntax error");
-
-    
+        )
+        .expect("Syntax error");
 
         println!("{:?}", tokens);
     }
 }
-
